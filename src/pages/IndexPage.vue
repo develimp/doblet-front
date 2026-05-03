ab765ae34d674cab0f65b3aa83ca0d92cc0fc853
Ivan Mas <ivmaspre@hotmail.com>
2026-05-03 19:14:19 +0200

    feat: :sparkles: refs #0104 update IndexPage to include PDF download button and implement download functionality

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Falla logo" src="~assets/falla-logo.png" style="max-width: 50%; height: auto" />
+  <q-page padding>
+    <q-btn
+      label="Document d'adhesió a la falla"
+      color="secondary"
+      icon="picture_as_pdf"
+      class="q-mr-sm"
+      @click="downloadPDF"
+    />
   </q-page>
 </template>
 
 <script setup>
-//
+import { api } from 'boot/axios'
+
+const downloadPDF = async () => {
+  try {
+    const response = await api.get('/members/pdf', {
+      responseType: 'blob',
+    })
+
+    const url = window.URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }))
+
+    window.open(url)
+  } catch (error) {
+    console.error('Error descargando PDF:', error)
+  }
+}
 </script>
