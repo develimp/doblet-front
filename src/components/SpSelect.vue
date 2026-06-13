bde8bb810d1dc3efe2605e6cfb289a68afef514c
develimp <ivmaspre@hotmail.com>
2026-06-13 23:40:57 +0200

    feat: refs #0110 implement authorization document handling in MemberPayment page

--- src/components/SpSelect.vue
+++ src/components/SpSelect.vue
@@ -1,42 +1,3 @@
-<!--
-  SpSelect
-  Wrapper de q-select amb estil unificat.
-
-  - class sp-select per a estilització comuna
-  - outlined es fixa ací
-  - use-input es fixa ací per a permetre cerca
-  - input-debounce es fixa a 300ms per a millorar rendiment en cerques
-  - clearable es fixa ací per a permetre netejar la selecció
-  - lazy-rules es fixa ací per a millorar rendiment en validacions
-  - options es una prop pròpia requerida (és el contingut del select)
-  - emit-value i map-options es fixen a true per a facilitar l'ús (es pot ficar a false en el cas que es necessite)
-  - $attrs es reenvía al q-select per a suportar qualsevol altre atribut de q-select
-  - filterFn implementa la lògica de filtratge per a la cerca
-  - model-value i update:model-value per a v-model i també es important per a compatibilitat amb q-select
--->
-<template>
-  <q-select
-    class="sp-select"
-    outlined
-    dense
-    use-input
-    input-debounce="300"
-    clearable
-    lazy-rules
-    :options="filteredOptions"
-    :emit-value="emitValue"
-    :map-options="mapOptions"
-    v-bind="$attrs"
-    :model-value="modelValue"
-    @filter="filterFn"
-    @update:model-value="(val) => emit('update:modelValue', val)"
-  >
-    <template #prepend>
-      <q-icon name="search" />
-    </template>
-  </q-select>
-</template>
-
 <script setup>
 import { ref, watch, useAttrs } from 'vue'
 
@@ -79,8 +40,32 @@ const filterFn = (val, update) => {
 }
 </script>
 
+<template>
+  <q-select
+    class="sp-select"
+    outlined
+    dense
+    use-input
+    input-debounce="300"
+    clearable
+    lazy-rules
+    :options="filteredOptions"
+    :emit-value="emitValue"
+    :map-options="mapOptions"
+    v-bind="$attrs"
+    :model-value="modelValue"
+    @filter="filterFn"
+    @update:model-value="(val) => emit('update:modelValue', val)"
+  >
+    <template #prepend>
+      <q-icon name="search" />
+    </template>
+  </q-select>
+</template>
+
 <style lang="scss">
 .sp-select {
+  width: 300px;
   max-width: 500px;
 }
 </style>
